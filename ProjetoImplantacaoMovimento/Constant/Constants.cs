4bba2a7dee61eeb9f6405f5408c6c41818bdfbe9
Gabriel Aceti <gabriel.aceti@hotmail.com>
2023-02-24 15:58:52 -0300

    Adicionando constantes para gridView

--- ProjetoImplantacaoMovimento/Constant/Constants.cs
+++ ProjetoImplantacaoMovimento/Constant/Constants.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.XtraGrid.Views.Grid;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,30 @@ namespace ProjetoImplantacaoMovimento.Constant
     public static class MovimentoDefaults
     {
         public const string MovimentoPath = "../../Data/Movimentos.txt";
+    }
+
+    public static class GridViewDefaults
+    {
+        public static GridView GridViewConfigurationDefaults(GridView gridView)
+        {
+            // Grid somente leitura
+            gridView.OptionsBehavior.Editable = false;
+
+            // Grid com seleção de multiplos registros
+            gridView.OptionsSelection.MultiSelect = true;
+            gridView.OptionsSelection.MultiSelectMode = GridMultiSelectMode.RowSelect;
+
+            // Grid com scroll horizontal
+            gridView.OptionsView.ColumnAutoWidth = false;
+
+            // Grid com auto ajuste do melhor tamanho das colunas
+            gridView.BestFitMaxRowCount = 800;
+            gridView.BestFitColumns();
+
+            // Grid mostrar coluna de filtro
+            gridView.OptionsView.ShowAutoFilterRow = true;
+
+            return gridView;
+        }
     }
 }
