e6b39cb5623253a69b97e5489e1acf280fa517be
Gabriel Aceti <gabriel.aceti@hotmail.com>
2023-03-01 09:25:11 -0300

    Adicionando campo observação no model do movimento

--- ProjetoImplantacaoMovimento/Models/Movimento.cs
+++ ProjetoImplantacaoMovimento/Models/Movimento.cs
@@ -11,6 +11,7 @@ namespace ProjetoImplantacaoMovimento.Models
         public string IdMovimento { get; set; }
         public string NumeroMovimento { get; set; }
         public string Descricao { get; set; }
+        public string Observacao { get; set; }
         public string CriadoPor { get; set; }
         public string CriadoEm { get; set; }
         public string ModificadoPor { get; set; }
